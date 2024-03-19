88d80c5ec119ef563bfa9d6b770eac474d22558e
Jacob Conklin <jacobyconklin@gmail.com>
2024-03-19 14:07:24 -0400

    New horizontal style for mechanical arm page

--- src/Pages/MechanicalArm/ArmExperiment/ArmExperiment.tsx
+++ src/Pages/MechanicalArm/ArmExperiment/ArmExperiment.tsx
@@ -32,10 +32,14 @@ const ArmExperiment = () => {
      
     */
 
-    const [showTypedMessage, setShowTypedMessage] = useState(true);
-    const [selectedSolver, setSelectedSolver] = useState(null);
-    const [selectedComponent, setSelectedComponent] = useState(null);
+    const [showTypedMessage, setShowTypedMessage] = useState(false);
+    const [selectedSolver, setSelectedSolver] = useState<string | null>(null);
+    const [selectedComponent, setSelectedComponent] = useState<string | null>(null);
+    const [selectedArchitecture, setSelectedArchitecture] = useState<string | null>(null);
     const [latestResult, setLatestResult] = useState("Electrical Engineer built the Entire Arm weighing 10kg and costing $1000");
+
+    // TODO need new types for mechanical arm results, solvers, etc. 
+    const [allResults, setAllResults] = useState<any[]>([]);
 
     // TODO use description as a tooltip
     const components = [
@@ -56,14 +60,105 @@ const ArmExperiment = () => {
         },
     ]
 
+    const architectures = [
+        {
+            name: "Entire Arm",
+            description: "This is the entire arm that will be built. This means one Solver type will be assigned to complete all aspects of the arm.",
+            components: ["Entire Arm"]
+        },
+        {
+            name: "Smart Arm and Base",
+            description: "This breaks construction down into developing a smart arm component and a simple base component.",
+            components: ["Smart Arm", "Base"]
+        },
+        {
+            name: "Arm and Smart Base",
+            description: "This breaks construction down into developing a smart base component and a simple arm component.",
+            components: ["Arm", "Smart Base"]
+        },
+        {
+            name: "Structure, Power, and Software",
+            description: "This breaks construction down into developing a mechanical structure, a power supply, a software system to handle major actions, and a software system to handle precise positioning of the arm",
+            components: ["Mechanical System", "Power Supply", "Action Software", "Positioning Software"]
+        }
+    ]
+
     return (
         <div className="ArmExperiment">
             <FactoryBackground />
             <div className='Instructions'>
                 <h1>Experimental Round</h1>
-                <p>
-                    In this round you can experiment with different solver types and components to see how they perform.
-                </p>
+
+                {/** Horizontal layout */}
+                <div className='HorizontalSections'>
+                    <div className='Architectures'>
+                        <h3>Pick An Architecture</h3>
+                        {
+                            // TODO add tooltip w/ descriptions
+                            architectures.map((architecture, index) => (
+                                <Button
+                                    onClick={() => setSelectedArchitecture(architecture.name)}
+                                    type={selectedArchitecture === architecture.name ? "primary" : "default"}
+                                >
+                                    {architecture.name}
+                                </Button>
+                            ))
+                        }
+                    </div>
+
+                    <div className='Components'>
+                        <h3>Pick A Component</h3>
+                        {
+                            selectedArchitecture &&
+                            <>
+                            {
+                                architectures.find(arch => arch.name === selectedArchitecture)?.components.map((component, index) => (
+                                    <Button
+                                        onClick={() => setSelectedComponent(component)}
+                                        type={selectedComponent === component ? "primary" : "default"}
+                                    >
+                                        {component}
+                                    </Button>
+                                ))
+                            }
+                            </>
+                        }
+                    </div>
+
+                    <div className='SolverSelection'>
+                        <h3>Pick A Solver Below</h3>
+                        {
+                            selectedSolver && 
+                            <>
+                                <p>
+                                    You selected {selectedSolver} 
+                                    {selectedComponent && ` to build the ${selectedComponent}`}
+                                </p>
+                                <img className='SelectedSolverImage' src={arm} alt='Solver Selected to build component' />
+                            </>
+                        }
+                    </div>
+                </div>
+
+                <div className='Actions'>
+                    <Button
+                        disabled={!selectedArchitecture || !selectedComponent || !selectedSolver}
+                    >
+                        Run Experiment
+                    </Button>
+                    <Button>
+                        View Results
+                    </Button>
+                </div>
+
+                {
+                    latestResult &&
+                    <p>Result: {latestResult}</p>
+                }
+
+
+                {/** Vertical layout */}
+                {/* 
                 <div className='Architectures'>
                     <Button>
                         Entire Arm
@@ -84,7 +179,6 @@ const ArmExperiment = () => {
                         components.map((component, index) => (
                             <div className='Component' key={index}>
                                 <h3>{component.name}</h3>
-                                {/* <p>{component.description}</p> */}
                                 <Button>Select</Button>
                                 <img className='ComponentImage' src={component.image} alt={component.name} />
                             </div>
@@ -109,7 +203,7 @@ const ArmExperiment = () => {
                 {
                     latestResult &&
                     <p>Result: {latestResult}</p>
-                }
+                } */}
             </div>
 
             <div className='SolverCards'>
@@ -118,39 +212,39 @@ const ArmExperiment = () => {
                     <img className='SolverImage' src={arm} alt='Electrical Engineer' />
                     <p>High voltage hero.</p>
                     <Button
-
+                        onClick={() => setSelectedSolver("Electrical Engineer")}
                     >
                         Select
                     </Button>
                 </div>
                 <div className='SolverCard'>
                     <h2>Materials Scientist</h2>
-                    <img className='SolverImage' src={arm} alt='Electrical Engineer' />
+                    <img className='SolverImage' src={arm} alt='Materials Scientist' />
                     <p>The stuff that matters.</p>
                     <Button
-
+                        onClick={() => setSelectedSolver("Materials Scientist")}
                     >
                         Select
                     </Button>
                 </div>
-                
+
                 <div className='SolverCard'>
                     <h2>Computer Scientist</h2>
-                    <img className='SolverImage' src={arm} alt='Electrical Engineer' />
+                    <img className='SolverImage' src={arm} alt='Computer Scientist' />
                     <p>Virtually an engineer.</p>
                     <Button
-
+                        onClick={() => setSelectedSolver("Computer Scientist")}
                     >
                         Select
                     </Button>
                 </div>
-                
+
                 <div className='SolverCard'>
                     <h2>Industrial Systems Engineer</h2>
-                    <img className='SolverImage' src={arm} alt='Electrical Engineer' />
+                    <img className='SolverImage' src={arm} alt='Industrial Systems Engineer' />
                     <p>Logistical legend.</p>
                     <Button
-
+                        onClick={() => setSelectedSolver("Industrial Systems Engineer")}
                     >
                         Select
                     </Button>
